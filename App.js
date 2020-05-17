6c5bdc797359f516b64b8eb7bad01ff05fb8a97f
Dmytro Portenko <troublesohard@gmail.com>
2020-05-17 17:50:03 +0300

    styles, custom modifiers

--- App.js
+++ App.js
@@ -3,19 +3,23 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow strict-local
+ * @flow
  */
 
 import React from 'react';
 
-import {View} from 'react-native-ui-lib';
+import {View, Text} from 'react-native-ui-lib';
+import { themeInit } from './src/themeInit';
+
+themeInit();
 
 const App: () => React$Node = () => {
   return (
-    <View flex bg-blue70 margin-20 padding-s1 row centerV spread>
+    <View flex bg-blue70 margin-screenOffset padding-s1 centerV spread>
       <View bg-red30 style={square} />
       <View bg-yellow30 style={square} />
       <View bg-green30 style={square} />
+      <Text h1 gold>Test text</Text>
     </View>
   );
 };
---
+++ src/themeInit.js
@@ -0,0 +1,22 @@
+/**
+ * @flow
+ * Created by Dima Portenko on 17.05.2020
+ */
+import {Typography, Colors, Spacings} from 'react-native-ui-lib';
+
+
+export const themeInit = () => {
+  Colors.loadColors({
+    pink: '#FF69B4',
+    gold: '#FFD700',
+  });
+
+  Typography.loadTypographies({
+    h1: {fontSize: 58, fontWeight: '300', lineHeight: 80},
+    h2: {fontSize: 46, fontWeight: '300', lineHeight: 64},
+  });
+
+  Spacings.loadSpacings({
+    screenOffset: 30,
+  })
+};
