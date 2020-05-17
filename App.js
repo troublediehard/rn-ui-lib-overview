dba0b0be4546c5613b64e5beaf6d2ae6fb9a00df
Dmytro Portenko <troublesohard@gmail.com>
2020-05-17 19:44:24 +0300

    add readme

--- App.js
+++ App.js
@@ -15,11 +15,13 @@ themeInit();
 
 const App: () => React$Node = () => {
   return (
-    <View flex bg-blue70 margin-screenOffset padding-s1 centerV spread>
-      <View bg-red30 style={square} />
-      <View bg-yellow30 style={square} />
-      <View bg-green30 style={square} />
-      <Text h1 gold>Test text</Text>
+    <View flex bg-blue70 margin-screenOffset padding-s1 centerV >
+      <Text center h1 blue10>RN UI LIB</Text>
+      <View row spread paddingT-40>
+        <View bg-red30 style={square} />
+        <View bg-yellow30 style={square} />
+        <View bg-green30 style={square} />
+      </View>
     </View>
   );
 };
---
+++ README.md
@@ -0,0 +1,6 @@
+Youtube video series about react-native-ui-lib UI library for React Native.
+https://youtu.be/mThMv_TGB0w 
+
+Library - https://github.com/wix/react-native-ui-lib
+
+<img src="./docs/screenshot.png" alt="demo" width="360"/>
